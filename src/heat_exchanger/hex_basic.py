b5d334b7e50ea928d9218d0f776edb9f59535dd1
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-25 16:08:50 +0100

    added basic transforms of 0d hex model

--- src/heat_exchanger/hex_basic.py
+++ src/heat_exchanger/hex_basic.py
@@ -1,2 +1,30 @@
-def dp(four_L_over_dh, G, one_over_rho_mean, friction_factor):
-    return 1 / 2 * G**2 * friction_factor * four_L_over_dh * one_over_rho_mean
+import numpy as np
+
+
+def dp(four_L_over_dh, mass_velocity, one_over_rho_mean, friction_factor):
+    return 1 / 2 * mass_velocity**2 * friction_factor * four_L_over_dh * one_over_rho_mean
+
+
+def ntu(
+    stanton_1,
+    stanton_2,
+    area_ratio_1_q_over_o,
+    area_ratio_2_q_over_o,
+    heat_capacity_flux_1,
+    heat_capacity_flux_2,
+):
+    c_min = np.minimum(heat_capacity_flux_1, heat_capacity_flux_2)
+    inv_ntu = (
+        1 / stanton_1 / area_ratio_1_q_over_o * c_min / heat_capacity_flux_1
+        + 1 / stanton_2 / area_ratio_2_q_over_o * c_min / heat_capacity_flux_2
+    )
+    return 1 / inv_ntu
+
+
+def epsilon(ntu, c_ratio):
+    """Calculate effectiveness of counterflow heat_exchanger"""
+    tol = 1e-9
+    if c_ratio < 1 - tol:
+        return 1 - np.exp(-ntu * (1 - c_ratio)) / (1 - c_ratio * np.exp(-ntu * (1 - c_ratio)))
+    else:
+        return ntu / (ntu + 1)
