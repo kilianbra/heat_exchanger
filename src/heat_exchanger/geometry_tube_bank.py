88d687bfcddb2abe082cd0becf45b73bc7a64dbb
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-31 16:02:42 +0100

    Added free flow area and heat transfer area calculations

--- src/heat_exchanger/geometry_tube_bank.py
+++ src/heat_exchanger/geometry_tube_bank.py
@@ -1,11 +1,8 @@
 import numpy as np
 
 
-def area_minimum_flow(area_frontal, sigma):
-    return sigma * area_frontal
-
-
 def sigma_tube_bank(tube_spacing_longitudinal_dimensionless):
+    """This is the ratio of minimum free flow area to frontal area"""
     return (tube_spacing_longitudinal_dimensionless - 1) / tube_spacing_longitudinal_dimensionless
 
 
@@ -13,5 +10,17 @@ def area_frontal_bank(total_diameter_outer, total_diameter_inner):
     return np.pi * (total_diameter_outer**2 - total_diameter_inner**2) / 4
 
 
+def area_free_flow_bank(area_frontal_bank, sigma_tube_bank):
+    return area_frontal_bank * sigma_tube_bank
+
+
+def area_free_flow_in_tubes(tube_diameter_inner, n_tubes_per_pass):
+    return np.pi * tube_diameter_inner**2 * n_tubes_per_pass
+
+
 def area_heat_transfer_bank(tube_diameter_outer, tube_length, n_rows, n_tubes_per_row):
     return np.pi * tube_diameter_outer * tube_length * n_rows * n_tubes_per_row
+
+
+def area_heat_transfer_in_tubes(tube_diameter_inner, tube_length, n_tubes):
+    return np.pi * tube_diameter_inner**2 * tube_length * n_tubes
