036e6f8c8b8f85c134b616d7422f2d929481b273
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-11-07 12:08:37 +0000

    Fixed print messages to format correctly

--- analysis/fluid_protocols_testing.py
+++ analysis/fluid_protocols_testing.py
@@ -7,25 +7,23 @@ if __name__ == "__main__":
     # logging levels  DEBUG < INFO < WARNING < ERROR < CRITICAL (Default is WARNING)
     configure_logging(logging.INFO)
 
-    print(
-        "Available perfect-gas presets: %s",
-        ", ".join(PerfectGasFluid.available_presets()),
-    )
-
     # Can now either initiaie
     air = PerfectGasFluid(M=28.97, S=111.0, T_ref=273.15, mu_ref=1.716e-5)
     air_state = air.state(T=600.0, P=2e5)
-    print("Air cp(T=600 K, P=2e5 Pa) = %.1f J/(kg·K)", air_state.cp)
-    print("Air mu(T=600 K, P=2e5 Pa) = %.2e Pa·s", air_state.mu)
+    print("Air cp(T=600 K, P=2e5 Pa) = {air_state.cp:.1f} J/(kg·K)")
+    print("Air mu(T=600 K, P=2e5 Pa) = {air_state.mu:.2e} Pa·s")
 
+    # Or use presets
     air = PerfectGasFluid.from_name("Air")
     air_state = air.state(T=600.0, P=2e5)
-    print("Air cp(T=600 K, P=2e5 Pa) = %.1f J/(kg·K)", air_state.cp)
-    print("Air mu(T=600 K, P=2e5 Pa) = %.2e Pa·s", air_state.mu)
+    print("Air cp(T=600 K, P=2e5 Pa) = {air_state.cp:.1f} J/(kg·K)")
+    print("Air mu(T=600 K, P=2e5 Pa) = {air_state.mu:.2e} Pa·s")
+
+    print("Available perfect-gas presets: " + ", ".join(PerfectGasFluid.available_presets()))
 
     hydrogen = PerfectGasFluid.from_name("H2")
     hydrogen_state = hydrogen.state(T=800.0, P=5e5)
-    print("Hydrogen rho(T=800 K, P=5e5 Pa) = %.2f kg/m^3", hydrogen_state.rho)
+    print("Hydrogen rho(T=800 K, P=5e5 Pa) = {hydrogen_state.rho:.2f} kg/m^3")
 
     try:
         PerfectGasFluid.from_name("Argon")
